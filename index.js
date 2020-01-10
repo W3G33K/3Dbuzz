33563cbdaf66941ed78fbd9f5cf66f6779956a3f
Ryan K. Clark <mail.w3geek@gmail.com>
2020-01-10 02:18:06 -0500

    seventh commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello, world!");
+let $ = require("cheerio"),
+	dotenv = require("dotenv"),
+	fs = require("fs"),
+	path = require("path");
+
+let envConfig = dotenv.config();
+if (envConfig.error) {
+	console.error(envConfig.error);
+}
+
+const THREEDBUZZ_HOME = process.env["3DBUZZ_HOME"];
+const THREEDBUZZ_URL = process.env["3DBUZZ_URL"];
+fs.readFile(THREEDBUZZ_URL, function(error, data) {
+	if (error) {
+		console.error(error);
+	}
+
+	let html = data.toString();
+	let jQ = $.load(html);
+	jQ("a").each(function(index, el) {
+		let self = jQ(el),
+			href = self.attr("href");
+		let filename = href.substr(href.lastIndexOf("/") + 1),
+			file = path.resolve(THREEDBUZZ_HOME, filename);
+		if (fs.existsSync(file) === false) {
+			console.error(`${file} does not exist!`);
+		}
+	});
+});
